8c15458bd6a8fbbb5e039e25abc87586a0b30aab
WasinV <114383385+WasinV@users.noreply.github.com>
2022-10-24 03:30:51 +0900

    If_Else_Basic1

---
+++ If Else 1.py
@@ -0,0 +1,18 @@
+# ifelse.py
+
+money = -300
+
+if money >= 300:
+	print('Go to Japanese restaurant')
+elif money >= 50:
+    print('Go to cheap Thai restaurant')
+elif money >= 0 and money < 50:
+	print('Go home')
+else:
+	print('Please fill out positive numbers')
+
+
+print('Go to study Python')
+
+#elif money >= 150:
+#	pass
